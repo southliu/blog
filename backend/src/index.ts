2e6203ea99eba2fe004964418da03d829cce78b0
South <1275093225@qq.com>
2021-08-23 23:00:55 +0800

    update: 允许跨域

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,17 +4,17 @@ import './controller/CommonController'
 import './controller/HomeController'
 const app = express()
 
-// app.all("*", function(request, response, next){
-//   response.header("Access-Control-Allow-Origin","*");
-//   //允许的header类型
-//   response.header("Access-Control-Allow-Headers","content-type");
-//   //跨域允许的请求方式
-//   response.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-//   if (request.method.toLowerCase() == 'options')
-//     response.send(200);  //让options尝试请求快速结束
-//   else
-//     next();
-// });
+app.all("*", function(request, response, next){
+  response.header("Access-Control-Allow-Origin","*");
+  //允许的header类型
+  response.header("Access-Control-Allow-Headers","content-type");
+  //跨域允许的请求方式
+  response.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  if (request.method.toLowerCase() == 'options')
+    response.send(200);  //让options尝试请求快速结束
+  else
+    next();
+});
 
 app.use(router)
 
