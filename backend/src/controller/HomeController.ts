4c232db3b6b9b01a39f80eaa33c2a2c87f0f85e0
South <1275093225@qq.com>
2021-08-29 23:50:31 +0800

    update: 分页接口

--- backend/src/controller/HomeController.ts
+++ backend/src/controller/HomeController.ts
@@ -3,16 +3,17 @@ import { controller, get } from "../decorator";
 import { IHomeResult } from "../types";
 import { handleResponse } from "../utils/utils";
 import connection from '../utils/connection'
+import { IPageDate } from '../types/global';
 
 @controller('/home')
 export class HomeController {
   @get('/page')
-  page(req: Request, res: Response) {
+  page(req: IPageDate, res: Response) {
     const { page, pageSize } = req.query
     console.log('page:', page)
     console.log('pageSize:', pageSize)
 
-    const sql = `SELECT * FROM home;`
+    const sql = `SELECT * FROM home LIMIT ${(page - 1) * pageSize},${pageSize};`
 
     connection.query(sql, function (err, result) {
       if (err) {
@@ -21,7 +22,17 @@ export class HomeController {
       }
       console.log(result);  //数据库查询结果返回到result中
       
-      res.json(handleResponse<IHomeResult[]>(200, result))
+      const countSql = `SELECT COUNT(id) as total FROM home;`
+      connection.query(countSql, function (countErr, countResult) {
+        if (countErr) {
+          console.log('错误信息:', countErr.message)
+          return res.json(handleResponse<string>(500, countErr.message))
+        }
+
+        console.log('countResult:', countResult)
+        result.count = countResult
+        res.json(handleResponse<IHomeResult[]>(200, result))
+      })
     })
   }
 }
---
+++ backend/src/types/global.d.ts
@@ -0,0 +1,9 @@
+import { Request } from "express";
+
+// 分页
+export interface IPageDate extends Request {
+  query: {
+    page: number;
+    pageSize: number;
+  }
+} 
