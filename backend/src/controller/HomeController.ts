5c8acb5dc515efd7fe7678dda23aba6525ef5067
刘南方 <1275093225@qq.com>
2021-08-30 13:49:18 +0800

    update: 优化page逻辑

--- backend/src/controller/HomeController.ts
+++ backend/src/controller/HomeController.ts
@@ -1,38 +1,32 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { controller, get } from "../decorator";
 import { IHomeResult } from "../types";
 import { handleResponse } from "../utils/utils";
 import connection from '../utils/connection'
 import { IPageDate } from '../types/global';
+import moment from 'moment'
 
 @controller('/home')
 export class HomeController {
   @get('/page')
   page(req: IPageDate, res: Response) {
-    const { page, pageSize } = req.query
-    console.log('page:', page)
-    console.log('pageSize:', pageSize)
+    let { page, pageSize } = req.query
+    page = page || 1
+    pageSize = pageSize || 20
 
-    const sql = `SELECT * FROM home LIMIT ${(page - 1) * pageSize},${pageSize};`
+    const sql = `SELECT * FROM home LIMIT ${(page - 1) * pageSize}, ${pageSize};
+      SELECT COUNT(id) as total FROM home;`
 
     connection.query(sql, function (err, result) {
       if (err) {
         console.log('错误信息:', err.message)
         return res.json(handleResponse<string>(500, err.message))
       }
-      console.log(result);  //数据库查询结果返回到result中
-      
-      const countSql = `SELECT COUNT(id) as total FROM home;`
-      connection.query(countSql, function (countErr, countResult) {
-        if (countErr) {
-          console.log('错误信息:', countErr.message)
-          return res.json(handleResponse<string>(500, countErr.message))
-        }
-
-        console.log('countResult:', countResult)
-        result.count = countResult
-        res.json(handleResponse<IHomeResult[]>(200, result))
-      })
+      // 过滤时间格式
+      const items = result[0].filter((item: IHomeResult) => 
+        item.date = moment(item.date).format('YYYY-MM-DD hh:mm:ss'))
+      const response = { items, ...result[1][0] }
+      res.json(handleResponse<IHomeResult[]>(200, response))
     })
   }
 }
--- backend/src/utils/connection.ts
+++ backend/src/utils/connection.ts
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
   port: 3306, // 端口号
   user: 'root', // 用户名
   password: 'root', // 密码
-  database: 'blog' // 数据库名称
+  database: 'blog', // 数据库名称
+  multipleStatements: true // 执行多行
 });
 
 export default connection
