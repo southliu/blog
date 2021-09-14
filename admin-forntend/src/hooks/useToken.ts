92fdf609a5c0509c0a4068129f72cfe53a01af3a
South <1275093225@qq.com>
2021-09-14 22:07:00 +0800

    update: useToken

--- admin-forntend/src/hooks/useToken.ts
+++ admin-forntend/src/hooks/useToken.ts
@@ -1,7 +1,12 @@
 import { TOKEN } from '../utils/config';
 import Cookies from 'js-cookie'
 
-function useToken() {
+function useToken(remove?: boolean) {
+  if (remove) {
+    Cookies.remove(TOKEN)
+    return ''
+  }
+
   const token = Cookies.get(TOKEN) || ''
 
   return token
