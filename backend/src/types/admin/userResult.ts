c1ec2240f3e94dd3294f9a704664097c1ead8a4d
刘南方 <1275093225@qq.com>
2021-09-22 15:16:36 +0800

    update: 用户数据

--- backend/src/types/admin/userResult.ts
+++ backend/src/types/admin/userResult.ts
@@ -1,7 +1,7 @@
 // 用户数据
 export interface IUserResult {
   id: number;
-  name: string;
-  role_ids: string;
-  date: string;
+  username: string;
+  password: string;
+  roleIds: string[] | number[]
 } 
