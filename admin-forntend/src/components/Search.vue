ec27390f76f89a719d074d4b106bf5bb98ec1038
South <1275093225@qq.com>
2021-09-26 23:38:32 +0800

    add: 搜索框

--- admin-forntend/src/components/Search.vue
+++ admin-forntend/src/components/Search.vue
@@ -1,13 +1,54 @@
 <template>
-  Search
+  <el-form
+    :inline="true"
+    :model="formData"
+    class="box"
+  >
+    <el-form-item
+      v-for="item in data"
+      :key="item.key"
+      :label="item.label"
+    >
+      <el-input placeholder="请输入" v-model="formData[item.key]"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">搜索</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'Search',
-  setup() {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    onSearch: {
+      type: Function,
+      required: true
+    }
+  },
+  setup(props) {
+    const formData = ref({})
+    // const formRef = ref()
+    const onSubmit = (): void | false => {
+      props.onSearch(formData.value)
+    }
+
+    return {
+      formData,
+      onSubmit
+    }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+  .box {
+    padding: 10px;
+  }
+</style>
